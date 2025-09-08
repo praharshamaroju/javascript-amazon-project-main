d22cf2db40e44de1f261afeefa55724dd26975ce
praharshamaroju <marojupraharsha@gmail.com>
2025-09-08 13:54:45 +0530

    delete products from HTML

--- javascript-amazon-project-main/scripts/checkout.js
+++ javascript-amazon-project-main/scripts/checkout.js
@@ -20,7 +20,8 @@ if(product.id === productId){
   
 cartsummaryHTML+=
 `
-<div class="cart-item-container">
+<div class="cart-item-container 
+js-cart-item-container-${matchingProduct.id}">
           <div class="delivery-date">
             Delivery date: Tuesday, June 21
           </div>
@@ -108,7 +109,11 @@ document.querySelectorAll('.js-delete-link').forEach((link)=>{
   link.addEventListener('click',()=>{
 const productId = link.dataset.productId;
 removeFromCart(productId);
-console.log(cart);
+
+
+const container = document.querySelector(`.js-cart-item-container-${productId}`);
+
+container.remove();
   });
 
 });
