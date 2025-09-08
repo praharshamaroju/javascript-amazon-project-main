23b22c44e091e5cebfffdb2b2c92265ad627e150
praharshamaroju <marojupraharsha@gmail.com>
2025-09-08 11:59:01 +0530

    fix price formatting and radio selector

--- javascript-amazon-project-main/scripts/checkout.js
+++ javascript-amazon-project-main/scripts/checkout.js
@@ -1,7 +1,7 @@
 import { products } from '../data/products.js';
 
 import { cart } from '../data/cart.js';
-  // correct relative path
+import { formatCurrency } from './utils/money.js';
 
 
 
@@ -34,7 +34,7 @@ cartsummaryHTML+=
                 ${matchingProduct.name}
               </div>
               <div class="product-price">
-                $${matchingProduct.priceCents/100}
+                $${formatCurrency(matchingProduct.priceCents)}
               </div>
               <div class="product-quantity">
                 <span>
@@ -57,7 +57,7 @@ cartsummaryHTML+=
               <div class="delivery-option">
                 <input type="radio" checked
                   class="delivery-option-input"
-                  name="delivery-option-1">
+                  name="delivery-option-${matchingProduct.id}">
                 <div>
                   <div class="delivery-option-date">
                     Tuesday, June 21
@@ -70,7 +70,7 @@ cartsummaryHTML+=
               <div class="delivery-option">
                 <input type="radio"
                   class="delivery-option-input"
-                  name="delivery-option-1">
+                  name="delivery-option-${matchingProduct.id}">
                 <div>
                   <div class="delivery-option-date">
                     Wednesday, June 15
@@ -83,7 +83,7 @@ cartsummaryHTML+=
               <div class="delivery-option">
                 <input type="radio"
                   class="delivery-option-input"
-                  name="delivery-option-1">
+                  name="delivery-option-${matchingProduct.id}">
                 <div>
                   <div class="delivery-option-date">
                     Monday, June 13
